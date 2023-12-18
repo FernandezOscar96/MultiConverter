5f7fc14f8dc9d13d1be7dee91cce2ff6c2fbcb00
FernandezOscar96 <oskyfernandezz.96@gmail.com>
2023-12-18 11:07:24 -0300

    modificaciones

--- src/com/conversor/datos/Datos.java
+++ src/com/conversor/datos/Datos.java
@@ -12,53 +12,62 @@ public class Datos {
 
 	public void setSeleccion(String seleccion) {
 		this.seleccion = seleccion;
-		switch (seleccion) {
-		case "peso - dolar":
-			trade.PesoAConvertir("dolar", moneda.getMonedas());
-			break;
-		case "peso - euro":
-			trade.PesoAConvertir("euro", moneda.getMonedas());
-			break;
-		case "peso - libra":
-			trade.PesoAConvertir("libra", moneda.getMonedas());
-			break;
-		case "peso - yen":
-			trade.PesoAConvertir("yen", moneda.getMonedas());
-			break;
-		case "peso - won":
-			trade.PesoAConvertir("won", moneda.getMonedas());
-			break;
-		case "dolar - peso":
-			trade.ConvertirAPeso("dolar", moneda.getMonedas());
-			break;
-		case "euro - peso":
-			trade.ConvertirAPeso("euro", moneda.getMonedas());
-			break;
-		case "libra - peso":
-			trade.ConvertirAPeso("libra", moneda.getMonedas());
-			break;
-		case "yuan - peso":
-			trade.ConvertirAPeso("yuan", moneda.getMonedas());
-			break;
-		case "won - peso":
-			trade.ConvertirAPeso("won", moneda.getMonedas());
-			break;
-		default:
-			break;
+		if (seleccion != null) {
+			switch (seleccion) {
+			case "peso - dolar":
+				trade.PesoAConvertir("dolar", moneda.getMonedas());
+				break;
+			case "peso - euro":
+				trade.PesoAConvertir("euro", moneda.getMonedas());
+				break;
+			case "peso - libra":
+				trade.PesoAConvertir("libra", moneda.getMonedas());
+				break;
+			case "peso - yen":
+				trade.PesoAConvertir("yen", moneda.getMonedas());
+				break;
+			case "peso - won":
+				trade.PesoAConvertir("won", moneda.getMonedas());
+				break;
+			case "dolar - peso":
+				trade.ConvertirAPeso("dolar", moneda.getMonedas());
+				break;
+			case "euro - peso":
+				trade.ConvertirAPeso("euro", moneda.getMonedas());
+				break;
+			case "libra - peso":
+				trade.ConvertirAPeso("libra", moneda.getMonedas());
+				break;
+			case "yuan - peso":
+				trade.ConvertirAPeso("yuan", moneda.getMonedas());
+				break;
+			case "won - peso":
+				trade.ConvertirAPeso("won", moneda.getMonedas());
+				break;
+			default:
+				break;
+			}
+		} else {
+			return;
 		}
-
 	}
 
 	public void setSeleccion2(String seleccion2) {
 		this.seleccion2 = seleccion2;
-		if (seleccion2 == "Fahrenheit a Celsius") {
-			double temp = Double
-					.parseDouble(JOptionPane.showInputDialog(null, "ingrese la temperatura a convertir: ", ""));
-			trade.ConvertirFaC(temp);
+		if (seleccion2 != null) {
+			if (seleccion2 == "Fahrenheit a Celsius") {
+				double temp = Double
+						.parseDouble(JOptionPane.showInputDialog(null, "ingrese la temperatura a convertir: ", ""));
+				trade.ConvertirFaC(temp);
+			} else {
+				double temp = Double
+						.parseDouble(JOptionPane.showInputDialog(null, "ingrese la temperatura a convertir: ", ""));
+				trade.ConvertirCaF(temp);
+			}
+
 		} else {
-			double temp = Double
-					.parseDouble(JOptionPane.showInputDialog(null, "ingrese la temperatura a convertir: ", ""));
-			trade.ConvertirCaF(temp);
+			return;
 		}
+
 	}
 }
--- src/com/conversor/main/Main.java
+++ src/com/conversor/main/Main.java
@@ -16,25 +16,29 @@ public class Main {
 			String opciones = (String) (JOptionPane.showInputDialog(null, "seleccione una opcion", "menu",
 					JOptionPane.QUESTION_MESSAGE, null,
 					new Object[] { "conversor de monedas", "conversor de temperatura" }, "seleccion"));
-			switch (opciones) {
-			case "conversor de monedas":
-				String input = (String) (JOptionPane.showInputDialog(null,
-						"seleccione a que tipo de moneda desea convertir: ", "monedas", JOptionPane.QUESTION_MESSAGE,
-						null,
-						new Object[] { "peso - dolar", "peso - euro", "peso - libra", "peso - yen", "peso - won",
-								"dolar - peso", "euro - peso", "libra - peso", "yuan - peso", "won - peso" },
-						"seleccion"));
-				datos.setSeleccion(input);
-				break;
-			case "conversor de temperatura":
-				String input2 = (String) (JOptionPane.showInputDialog(null, "seleccione a que tipo desea convertir: ",
-						"temperatura", JOptionPane.QUESTION_MESSAGE, null,
-						new Object[] { "Fahrenheit a Celsius", "Celsius a Fahrenheit" }, "seleccion"));
-				datos.setSeleccion2(input2);
-				break;
-			default:
-				break;
+			if (opciones != null) {
+				switch (opciones) {
+				case "conversor de monedas":
+					String input = (String) (JOptionPane.showInputDialog(null,
+							"seleccione a que tipo de moneda desea convertir: ", "monedas",
+							JOptionPane.QUESTION_MESSAGE, null,
+							new Object[] { "peso - dolar", "peso - euro", "peso - libra", "peso - yen", "peso - won",
+									"dolar - peso", "euro - peso", "libra - peso", "yuan - peso", "won - peso" },
+							"seleccion"));
+					datos.setSeleccion(input);
+					break;
+				case "conversor de temperatura":
+					String input2 = (String) (JOptionPane.showInputDialog(null,
+							"seleccione a que tipo desea convertir: ", "temperatura", JOptionPane.QUESTION_MESSAGE,
+							null, new Object[] { "Fahrenheit a Celsius", "Celsius a Fahrenheit" }, "seleccion"));
+					datos.setSeleccion2(input2);
+					break;
+				default:
+					break;
 
+				}
+			} else {
+				return;
 			}
 		}
 
